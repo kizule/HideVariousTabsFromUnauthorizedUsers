fb1968cbb38a8ec792e4d46162061ff9441125e6
Zoran Dori <zorandori4444@gmail.com>
2022-08-10 00:37:01 +0200

    My first extension, let's give it a try

---
+++ Gruntfile.js
@@ -0,0 +1,20 @@
+/* eslint-env node */
+module.exports = function ( grunt ) {
+	grunt.loadNpmTasks( 'grunt-eslint' );
+
+	grunt.initConfig( {
+		eslint: {
+			options: {
+				cache: true
+			},
+			all: [
+				'**/*.{js,json}',
+				'!node_modules/**',
+				'!vendor/**'
+			]
+		},
+	} );
+
+	grunt.registerTask( 'test', [ 'eslint' ] );
+	grunt.registerTask( 'default', 'test' );
+};
---
+++ src/HideVariousTabsFromUnauthorizedUsers.php
@@ -0,0 +1,32 @@
+<?php
+
+class HideVariousTabsFromUnauthorizedUsers {
+	/**
+	 * @param SkinTemplate &$sktemplate
+	 * @param array &$links
+	 * @return bool
+	 */
+	public static function onSkinTemplateNavigation( SkinTemplate &$sktemplate, array &$links ) {
+		global $wgTabsToRemove;
+		$user = RequestContext::getMain()->getUser();
+
+		// Only remove tabs if user isn't allowed to edit pages
+		if ( $user->isAllowed( 'edit' ) ) {
+			return false;
+		}
+
+		// Remove talkpage tab
+		if ( isset( $links['namespaces']['talk'] ) ) {
+			unset( $links['namespaces']['talk'] );
+		}
+
+		// Remove actions tabs
+		foreach ( $wgTabsToRemove as $view ) {
+			if ( isset( $links['views'][$view] ) ) {
+				unset( $links['views'][$view] );
+			}
+		}
+
+		return true;
+	}
+}
